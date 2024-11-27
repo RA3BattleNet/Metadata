21033a07d4104dd7d15254bc3b33418710888763
mrbbbaixue <mrbbbaixue@outlook.com>
2024-11-27 13:52:10 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,1 +1,11 @@
-# 读取XML文件示例
+import os
+
+# 获取环境变量
+commit_sha = os.getenv('CF_PAGES_COMMIT_SHA')
+
+# 如果环境变量存在，则将其写入文件
+if commit_sha:
+    with open('commit.txt', 'w') as file:
+        file.write(commit_sha)
+else:
+    print("环境变量 'CF_PAGES_COMMIT_SHA' 未设置。")
